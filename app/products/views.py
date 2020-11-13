04414cb033e07643e64febc02594defcb405351d
mguerrerogir <mariacamila.guerrerogiraldo@mercadolibre.com.co>
2020-11-12 22:48:46 -0500

    [ADD] Refactor views and added homework

--- app/products/views.py
+++ app/products/views.py
@@ -1,43 +1,46 @@
+import sys
 from http import HTTPStatus
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, render_template
 
-from app.products.models import get_all_categories, create_new_category, \
-    get_all_products, get_product_by_id
+from app.products.models import (
+    get_all_categories,
+    create_new_category,
+    get_all_products,
+    get_product_by_id,
+    create_product,
+    get_stock_by_product,
+    get_products_with_low_stock,
+)
 
-products = Blueprint("products", __name__, url_prefix='/products')
+products = Blueprint("products", __name__, url_prefix="/products")
 
 EMPTY_SHELVE_TEXT = "Empty shelve!"
 PRODUCTS_TITLE = "<h1> Products </h1>"
 DUMMY_TEXT = "Dummy method to show how Response works"
-RESPONSE_BODY = {
-    "message": "",
-    "data": [],
-    "errors": [],
-    "metadata": []
-}
+RESPONSE_BODY = {"message": "", "data": [], "errors": [], "metadata": []}
 
 
-@products.route('/dummy-product', methods=['GET', 'POST'])
+@products.route("/dummy-product", methods=["GET", "POST"])
 def dummy_product():
-    """ This method test the request types. If is GET Type it will
+    """This method test the request types. If is GET Type it will
     render the text Products in h1 label with code 500.
     If is POST Type it will return Empty shelve! with status code 403
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         return EMPTY_SHELVE_TEXT, HTTPStatus.FORBIDDEN
 
     return PRODUCTS_TITLE, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@products.route('/dummy-product-2')
+@products.route("/dummy-product-2")
 def dummy_product_two():
-    """ This method shows how Response object could be used to make API
+    """This method shows how Response object could be used to make API
     methods.
     """
     return Response(DUMMY_TEXT, status=HTTPStatus.OK)
 
 
-@products.route('/categories')
+@products.route("/categories")
 def get_categories():
     """
         Verificar que si get_all_categories es [] 400, message = "No hay nada"
@@ -57,7 +60,7 @@ def get_categories():
     return RESPONSE_BODY, status_code
 
 
-@products.route('/add-category', methods=['POST'])
+@products.route("/add-category", methods=["POST"])
 def create_category():
     """
 
@@ -75,7 +78,7 @@ def create_category():
     return RESPONSE_BODY, status_code
 
 
-@products.route('/')
+@products.route("/")
 def get_products():
     products_obj = get_all_products()
 
@@ -85,7 +88,7 @@ def get_products():
     return RESPONSE_BODY, 200
 
 
-@products.route('/product/<int:id>')
+@products.route("/product/<int:id>")
 def get_product(id):
     product = get_product_by_id(id)
 
@@ -93,16 +96,39 @@ def get_product(id):
     return RESPONSE_BODY, 200
 
 
-@products.route('/product-stock/<int:id>')
+@products.route("/product-stock/<int:product_id>")
 def get_product_stock(product_id):
-    pass
+    product_stock = get_stock_by_product(product_id)
+    RESPONSE_BODY["message"] = "Product stock"
+    RESPONSE_BODY["data"] = product_stock
+
+    return RESPONSE_BODY, HTTPStatus.OK
 
 
-@products.route('/need-restock')
+@products.route("/need-restock")
 def get_products_that_need_restock():
-    pass
+    products_low_stock = get_products_with_low_stock()
+    RESPONSE_BODY["message"] = "This products need to be re-stocked"
+    RESPONSE_BODY["data"] = products_low_stock
+
+    return RESPONSE_BODY, HTTPStatus.OK
 
 
-@products.route('/register-product-stock/<int:id>', methods=['PUT', 'POST'])
+@products.route("/register-product-stock/<int:id>", methods=["PUT", "POST"])
 def register_product_refund_in_stock():
-    pass
+
+    # TODO Complete this view to update stock for product when a register for
+    # this products exists. If not create the new register in DB
+
+    status_code = HTTPStatus.CREATED
+    if request.method == "PUT":
+        RESPONSE_BODY["message"] = \
+            "Stock for this product were updated successfully!"
+        status_code = HTTPStatus.OK
+    elif request.method == "POST":
+        RESPONSE_BODY["message"] = \
+            "Stock for this product were created successfully!"
+        pass
+    else:
+        RESPONSE_BODY["message"] = "Method not Allowed"
+        status_code = HTTPStatus.METHOD_NOT_ALLOWED
